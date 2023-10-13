42cab82d07533bbd77d731f4666ef29f324bce37
georgetian <georgetian@kuaishou.com>
2023-10-13 17:28:30 +0800

    update map

--- lib/widgets/settings.dart
+++ lib/widgets/settings.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_baidu_mapapi_map/flutter_baidu_mapapi_map.dart';
 import 'package:flutter_baidu_mapapi_base/flutter_baidu_mapapi_base.dart';
-import 'package:flutter_baidu_mapapi_utils/flutter_baidu_mapapi_utils.dart';
-import 'package:flutter_bmflocation/flutter_bmflocation.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -11,21 +9,52 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  late final BMFMapController mapController;
+  late final BMFMapWidget map;
+  final BMFCircle circle = BMFCircle(
+      center: BMFCoordinate(39.917215, 116.380341),
+      radius: 10,
+      width: 0,
+      strokeColor: Colors.transparent,
+      fillColor: Color.fromARGB(128, 255, 0, 0),
+      lineDashType: BMFLineDashType.LineDashTypeSquare);
+
   @override
-  build(BuildContext context) {
-    
-    BMFMapOptions mapOptions = BMFMapOptions(
+  void initState() {
+    super.initState();
+    map = BMFMapWidget(
+      onBMFMapCreated: (controller) async {
+        mapController = controller;
+        mapController.showUserLocation(true);
+        print('mapController#########################################');
+        print(mapController);
+        print(controller);
+      },
+      mapOptions: BMFMapOptions(
         center: BMFCoordinate(39.917215, 116.380341),
         zoomLevel: 12,
-        mapPadding: BMFEdgeInsets(left: 30, top: 0, right: 30, bottom: 0));
-    return Container(
-      constraints: const BoxConstraints.expand(),
-      child: BMFMapWidget(
-        onBMFMapCreated: (controller) {
-          print('onBMFMapCreated');
-        },
-        mapOptions: mapOptions,
+        trafficEnabled: true,
+        compassEnabled: true,
+        showMapPoi: true,
+        buildingsEnabled: true,
+        baseIndoorMapEnabled: true,
+        showIndoorMapPoi: true,
       ),
+    );
+  }
+
+  @override
+  build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [map],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () async {
+        print('onpressed');
+        bool result = await mapController.addCircle(circle);
+        print(result);
+      }),
     );
   }
 }
